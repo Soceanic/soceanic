b4396d2b73acc4a763c997d02a37be466249ddda
William D Posey Jr <sirwposeyjr@gmail.com>
2017-04-05 20:25:06 -0500

    Update profilepage.php

--- backend/src/profilepage.php
+++ backend/src/profilepage.php
@@ -1,1 +1,22 @@
+<?php
 
+$app->get('/users/id', function($username = null) {
+    $sqlGetUser = $pdo->prepare(
+      'SELECT * FROM users WHERE username = ?'
+      );
+    $sqlGetUser->execute([$username]);
+    $user = $sqlGetUser->fetch();  
+  
+    $sqlVerifyUser = $pdo->prepare(
+      'SELECT COUNT(*) FROM users WHERE username = ?'
+      );
+    $sqlVerifyUser->execute([$username]);
+    $userCount = $sqlVerifyUser->fetch(); 
+  
+    if (!$username->isEmpty()) {
+        if ($userCount > 0) {
+           
+        }
+    }
+  }
+);
