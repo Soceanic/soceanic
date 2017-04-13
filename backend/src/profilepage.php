095ddbfd7af17545ec54dac63c2d7db8356d6249
William D Posey Jr <sirwposeyjr@gmail.com>
2017-04-12 20:28:26 -0500

    revised /GET to have status numbers and also not be stupid

--- backend/src/profilepage.php
+++ backend/src/profilepage.php
@@ -2,29 +2,22 @@
 // Routes for the profile page
 
 // Get takes in a username and returns a user's info as Json
-$app->get('/users/id', function($username = null) {  
-    // Ensure the $username field is full
-    if (!$username->isEmpty()) {
-        // This counts how many users there are with that username
-        // to verify that the user actually exists
-        $sqlVerifyUser = $pdo->prepare(
-            'SELECT COUNT(*) FROM users WHERE username = ?'
-        );
-        $sqlVerifyUser->execute([$username]);
-        $userCount = $sqlVerifyUser->fetch();
-        
-        if ($userCount == 1) {
-            // This sequence stores the user in $user
-            $sqlGetUser = $pdo->prepare(
-                'SELECT username, first_name, last_name, profile_pic, bg_pic, bio, birthday
-                FROM users WHERE username = ?'
-            );
-            $sqlGetUser->execute([$username]);
-            $user = $sqlGetUser->fetch();
-           return $this->response->withJson($user);
-        }
+$app->get('/users/id', function($username, $response) {  
+    // Ensure the $username field is populated
+    if (!$isset($username) {
+	return $response->withStatus(418);    
     }
-  }
+        
+    // This sequence stores the user in $user
+    $sqlGetUser = $pdo->prepare(
+    	'SELECT username, first_name, last_name, profile_pic, bg_pic, bio, birthday
+          FROM users WHERE username = ?'
+    );
+    $sqlGetUser->execute([$username]);
+    $user = $sqlGetUser->fetch();
+	
+    return $this->response->withJson($user, 302);
+    }
 );
 
 $app->delete('/users/id', function($username = null) { 
@@ -43,15 +36,13 @@ $app->delete('/users/id', function($username = null) {
 				'DELETE FROM users WHERE username = ?'
 			);
 			$sqlGetUser->execute([$username]);
-		}elseif($userCount == 0){
-			return "The user does not exist.";
 		}
 		$sqlVerifyUser->execute([$username]);
 		$userCount = $sqlVerifyUser->fetch();
 		if ($userCount == 0){
-			return "The user is deleted.";
+			echo "The user is deleted.";
 		}else {
-			return "The deleting function failed.";
+			echo "The deleting function failed.";
 		}
 	}
 }); 
