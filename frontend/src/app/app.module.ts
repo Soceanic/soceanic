b1941806799882a031c798c16d8bfba463b04624
Andrew Ksendzoff <dacoconuttman@gmail.com>
2017-04-21 23:30:19 -0500

    split frontend up into modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RegistrationComponent } from './registration/registration.component';
-
-const routes: Routes = [
-  { path: '', component: LandingPageComponent }
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingPageComponent,
-    RegistrationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    HttpModule,
-    RouterModule.forRoot(routes)
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
