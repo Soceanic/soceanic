af49c30538ef56ef708b04d1d530e89b073e9f35
Jenn Le <jenn.thakugan@icloud.com>
2017-04-22 06:57:49 -0500

    Test image upload

---
+++ backend/src/helper.php
@@ -0,0 +1,42 @@
+<?php
+// Assistive functions
+require '../vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Common\Aws;
+use Aws\Exception\AwsException;
+use \Firebase\JWT\JWT;
+
+function validated_user($jwt) {
+  $username = NULL;
+
+  try {
+    $decoded = JWT::decode($jwt, $_SERVER['SECRET_KEY'], array('HS256'));
+    $username = $decoded->username;
+  } catch (Exception $e) {
+    echo "Exception: " . $e->getMessage();
+  }
+
+  return $username;
+}
+
+// Upload image to aws bucket
+function upload_image($path, $name) {
+  //Create a S3Client
+  $aws = Aws::factory();
+  $client = $aws->get('S3');
+
+  $result = $client->putObject(array(
+    'Bucket'     => $bucket,
+    'Key'        => $name . '.png',
+    'SourceFile' => $path,
+  ));
+
+  // We can poll the object until it is accessible
+  $client->waitUntil('ObjectExists', array(
+    'Bucket' => $this->bucket,
+    'Key'    => $name . '.png',
+  ));
+
+  return $result['ObjectURL'];
+}
