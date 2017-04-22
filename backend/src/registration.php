352e911081f43be60cb165283979382d48d420a7
Jenn Le <jenn.thakugan@icloud.com>
2017-04-21 23:56:53 -0500

    token test

--- backend/src/registration.php
+++ backend/src/registration.php
@@ -98,7 +98,7 @@ $app->post('/user', function ($request, $response, $args) {
 });
 
 // Validating a user's email
-$app->get('/token/[{token}]', function ($request, $response, $args) {
+$app->get('/token/{token}', function ($request, $response, $args) {
     echo $args['token'];
     try {
       $decoded = JWT::decode($args['token'], $_SERVER['SECRET_KEY'], array($_SERVER['ALGORITHM']));
