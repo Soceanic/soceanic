386ca57c10cb584ec64aaae2d457f28625330730
Andrew Ksendzoff <dacoconuttman@gmail.com>
2017-04-22 00:41:53 -0500

    scaffolded most services, wrote registration service

--- frontend/src/app/landing/landing.module.ts
+++ frontend/src/app/landing/landing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Registration } from '../services/objects/registration';
+import { RegistrationService } from '../services/registration.service';
+import { RegistrationModalComponent } from './registration-modal/registration-modal.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RegistrationModalComponent],
+  providers: [
+    Registration,
+    RegistrationService
+  ]
 })
 export class LandingModule { }
---
+++ frontend/src/app/landing/registration-modal/registration-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-modal',
+  templateUrl: './registration-modal.component.html',
+  styleUrls: ['./registration-modal.component.css']
+})
+export class RegistrationModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
