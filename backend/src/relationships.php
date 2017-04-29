65ca72d8258439c13765880fd3d36a407fb19aa3
Jenn Le <jenn.thakugan@icloud.com>
2017-04-29 14:17:50 -0500

    A lot of relationship routes

--- backend/src/relationships.php
+++ backend/src/relationships.php
@@ -48,7 +48,7 @@ $app->put('/friend', function ($request, $response, $args) {
     }
 
     $stmt = $pdo->prepare('UPDATE Relationships SET status=1, last_updated=CURRENT_TIMESTAMP
-      WHERE username=:username');
+      WHERE username_2=:username');
 
     $stmt->bindParam("username", $username);
     $stmt->execute();
@@ -63,24 +63,101 @@ $app->put('/ignore', function ($request, $response, $args) {
     $json = $request->getBody();
     $data = json_decode($json);
 
-// Block
+    $username = $data->username;
+
+    // Check if json is valid
+    if( !isset($username) || !isset($username)) {
+
+      return $response->withStatus(418);
+    }
+
+    $stmt = $pdo->prepare('UPDATE Relationships SET status=2, last_updated=CURRENT_TIMESTAMP
+      WHERE username_2=:username');
+
+    $stmt->bindParam("username", $username);
+    $stmt->execute();
+
+    return $response->withStatus(200);
 });
 
+// Block a friend
+$app->put('/block', function ($request, $response, $args) {
+    $pdo = $this->db;
+    $json = $request->getBody();
+    $data = json_decode($json);
+
+    $username = $data->username;
+
+    // Check if json is valid
+    if( !isset($username) || !isset($username)) {
+
+      return $response->withStatus(418);
+    }
+
+    $stmt = $pdo->prepare('UPDATE Relationships SET status=3, last_updated=CURRENT_TIMESTAMP
+      WHERE username_2=:username');
+
+    $stmt->bindParam("username", $username);
+    $stmt->execute();
+
+    return $response->withStatus(200);
+
+});
+
+// Block a stranger
 $app->post('/block', function ($request, $response, $args) {
     $pdo = $this->db;
     $json = $request->getBody();
     $data = json_decode($json);
 
-// Getting a list of the user's incoming or outgoing requests
+    $username1 = $data->username1;
+    $username2 = $data->username2;
+
+    // Check if json is valid
+    if( !isset($username1) || !isset($username2) ||
+        empty($username1) || empty($username2)) {
+
+      return $response->withStatus(418);
+    }
+
+    $stmt = $pdo->prepare(
+      "INSERT INTO Relationships (username_1, username_2, status, date_sent,
+         last_updated, group_id_1, group_id_2)
+       VALUES (:username1, :username2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+         0, 0)"
+      );
+
+    $stmt->bindParam("username1", $username1);
+    $stmt->bindParam("username2", $username2);
+    $stmt->execute();
+
+    return $response->withStatus(201);
+});
+
+// Getting a list of the user's incoming requests
 $app->get('/requests', function ($request, $response, $args) {
     $pdo = $this->db;
     $json = $request->getBody();
     $data = json_decode($json);
 
+    $username = $data->username;
+
+    // Check if json is valid
+    if( !isset($username) || !isset($username)) {
+      return $response->withStatus(418);
+    }
+
+    $stmt = $pdo->prepare('SELECT username_1, status, date_sent FROM Relationships
+      WHERE username_2=:username');
+
+    $stmt->bindParam("username", $username);
+    $stmt->execute();
+
+    return $response->withStatus(200);
 });
 
 // Get status of a relationship
-$app->post('/status', function ($request, $response, $args) {
+$app->get('/status', function ($request, $response, $args) {
     $pdo = $this->db;
     $json = $request->getBody();
     $data = json_decode($json);
