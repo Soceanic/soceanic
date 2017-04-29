d8467cf0e1c59d7183c0cee0c4e6ef49f9ed6eba
Jenn Le <jenn.thakugan@icloud.com>
2017-04-29 17:50:12 -0500

    Kill me

--- backend/public/index.php
+++ backend/public/index.php
@@ -27,10 +27,11 @@ require __DIR__ . '/../src/middleware.php';
 // Register all routes files ---------------------------------------------------------
 require __DIR__ . '/../src/login.php';
 require __DIR__ . '/../src/static_posts.php';
-require __DIR__ . '/../src/relationships.php';
+require __DIR__ . '/../src/static_relationships.php';
 // Put all static routes above this line cause fuck php and slim in particular
 require __DIR__ . '/../src/registration.php';
 require __DIR__ . '/../src/posts.php';
+require __DIR__ . '/../src/relationships.php';
 require __DIR__ . '/../src/routes.php';
 
 // Run app
