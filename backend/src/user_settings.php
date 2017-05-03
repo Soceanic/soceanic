1c4335eff4922656949a5d17756a616e1d54cb0a
Jenn Le <jenn.thakugan@icloud.com>
2017-05-03 12:47:40 -0500

    Delete user

--- backend/src/user_settings.php
+++ backend/src/user_settings.php
@@ -10,44 +10,44 @@ $app->put('/user', function ($request, $response, $args) {
     $birthday = $data->birthday;
     $email = $data->email;
     $plain_password = $data->password;
-    
-    if (isset($first_name)) {
-      $stmt = $pdo->prepare( 
+
+    if (isset($first_name) && !empty($first_name)) {
+      $stmt = $pdo->prepare(
           "UPDATE Users SET first_name=:first_name WHERE username=:username"
           );
       $stmt->bindParam("username", $username);
       $stmt->bindParam("first_name", $first_name);
       $stmt->execute();
     }
-    
-    if (isset($last_name)) {
-      $stmt = $pdo->prepare( 
+
+    if (isset($last_name) && !empty($last_name)) {
+      $stmt = $pdo->prepare(
           "UPDATE Users SET last_name=:last_name WHERE username=:username"
           );
       $stmt->bindParam("username", $username);
       $stmt->bindParam("last_name", $last_name);
-      $stmt->execute();  
+      $stmt->execute();
     }
-    
-    if (isset($birthday)) {
-      $stmt = $pdo->prepare( 
+
+    if (isset($birthday) && !empty($birthday)) {
+      $stmt = $pdo->prepare(
           "UPDATE Users SET birthday=:birthday WHERE username=:username"
           );
       $stmt->bindParam("username", $username);
       $stmt->bindParam("birthday", $birthday);
       $stmt->execute();
     }
-    
-    if (isset($email)) {
-      $stmt = $pdo->prepare( 
+
+    if (isset($email) && !empty($email)) {
+      $stmt = $pdo->prepare(
           "UPDATE Users SET email=:email WHERE username=:username"
           );
       $stmt->bindParam("username", $username);
       $stmt->bindParam("email", $email);
       $stmt->execute();
     }
-    
-    if (isset($password)) {
+
+    if (isset($plain_password) && !empty($plain_password)) {
       $is_valid = password_verify($plain_password, $password);
       if($is_valid) {
         // Rehash the password if necessary
@@ -63,4 +63,24 @@ $app->put('/user', function ($request, $response, $args) {
     }
 
     return $response->withStatus(201);
+});
+
+$app->delete('/user', function ($request, $response, $args) {
+    $pdo = $this->db;
+    $json = $request->getBody();
+    $data = json_decode($json);
+    $username = $data->username;
+
+    if( !isset($username) || empty($username)) {
+
+      return $response->withStatus(418);
+    }
+
+    $stmt = $pdo->prepare(
+        "DELETE FROM Users WHERE username=:username"
+        );
+    $stmt->bindParam("username", $username);
+    $stmt->execute();
+
+    return $response->withStatus(204);
 });
